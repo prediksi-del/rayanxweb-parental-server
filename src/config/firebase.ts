fbb60cdc798f5642f6a6ee001c7414573be6a3c3
prediksi-del <kematianituada493@gmail.com>
2026-05-20 04:58:47 +0700

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,21 +2,22 @@ import * as admin from 'firebase-admin';
 
 if (!admin.apps.length) {
   try {
-    // Membaca string JSON rahasia dari Environment Variable Vercel
     const firebaseServiceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
 
     if (firebaseServiceAccount) {
       const serviceAccount = JSON.parse(firebaseServiceAccount);
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
+        // WAJIB: Masukkan URL Realtime Database Anda di bawah ini
+        databaseURL: "https://steadfast-tesla-402305/database/steadfast-tesla-402305-default-rtdb"
       });
-      console.log('Firebase Admin SDK Production berhasil diinisialisasi.');
+      console.log('Firebase Admin SDK Production & RTDB berhasil terhubung.');
     } else {
-      console.warn('Peringatan: FIREBASE_SERVICE_ACCOUNT tidak ditemukan di Environment Variables.');
+      console.warn('FIREBASE_SERVICE_ACCOUNT tidak ditemukan.');
     }
   } catch (error) {
-    console.error('Gagal memuat kredensial Firebase di Production:', error);
+    console.error('Gagal memuat kredensial Firebase:', error);
   }
 }
 
-export const messaging = admin.apps.length ? admin.messaging() : null;
+export const rtdb = admin.apps.length ? admin.database() : null;
