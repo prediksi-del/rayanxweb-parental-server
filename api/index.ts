deecdcc55ab9a766a601cfa311358fe7b817e1d3
prediksi-del <kematianituada493@gmail.com>
2026-05-20 04:25:56 +0700

    Initialize server with dotenv and database connection

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import app from '../src/app';
+import { connectDB } from '../src/config/db';
+
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+      console.log(`Server berjalan lokal pada port ${PORT}`);
+    });
+  }
+});
+
+export default app;
